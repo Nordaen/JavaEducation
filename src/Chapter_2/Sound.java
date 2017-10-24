027372fc89007f3309e6e57b10b561a14856f118
Vlad Kondratev <sykidayteadres@gmail.com>
2017-10-24 14:15:07 +0300

    новый пример

---
+++ src/Chapter_2/Sound.java
@@ -0,0 +1,10 @@
+package Chapter_2;
+
+public class Sound {
+    public static void sound()
+    {
+        double distance;
+        distance = 1100 *7.2;
+        System.out.println("Расстояние до места вспышки молнии составляет " + distance + " футов");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-import Chapter_2.BoolDemo;
-import Chapter_2.CharArithDemo;
-import Chapter_2.Hypot;
-import Chapter_2.Inches;
+import Chapter_2.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         //kekeke
+        String s = "-------------------";
         Inches.inches();
+        System.out.println(s);
+
         Hypot.hypot();
+        System.out.println(s);
+
         CharArithDemo.chararithdemo();
+        System.out.println(s);
+
         BoolDemo.booldemo();
+        System.out.println(s);
+
+        Sound.sound();
+        System.out.println(s);
     }
 
 
