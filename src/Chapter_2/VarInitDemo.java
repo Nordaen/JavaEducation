e4475bb1410f456eea29a99a7a27923e5085bf45
Vlad Kondratev <sykidayteadres@gmail.com>
2017-10-26 16:45:41 +0300

    1

--- src/Chapter_2/VarInitDemo.java
+++ src/Chapter_2/VarInitDemo.java
@@ -1,4 +1,18 @@
 package Chapter_2;
 
 public class VarInitDemo {
+    public static void varinitdemo()
+
+    {
+        int x;
+
+        for (x=0; x<3; x++)
+        {
+            int y = -1;
+            System.out.println("y:" + y);
+            y=100;
+            System.out.println("Changed y: "+ y);
+            
+        }
+    }
 }
