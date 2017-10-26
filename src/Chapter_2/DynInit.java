b02e0842a01aeab0c78bfe86c17a781d62a02589
Vlad Kondratev <sykidayteadres@gmail.com>
2017-10-26 14:05:54 +0300

    Новые изменения

---
+++ src/Chapter_2/DynInit.java
@@ -0,0 +1,13 @@
+package Chapter_2;
+
+public class DynInit {
+    public static void dyninit()
+    {
+        double radius = 4, height = 5;
+
+        double volume = 3.1416 * radius * radius * height;
+
+        System.out.println("Объем: " + volume);
+    }
+
+}
