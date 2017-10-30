dc12a907db52e8952f2d24d09feabebc1a91916b
Vlad Kondratev <sykidayteadres@gmail.com>
2017-10-30 14:12:53 +0200

    Новые изменения

---
+++ src/Chapter_2/PromDemo.java
@@ -0,0 +1,15 @@
+package Chapter_2;
+
+public class PromDemo {
+    public static void promdemo()
+    {
+        byte b;
+        int i;
+
+        b = 10;
+        i = b * b;
+        b = (byte) (b *b );
+
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
