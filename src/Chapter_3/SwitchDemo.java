0d83eb761598370fcc18208128a1a08d7fda36de
Vlad Kondratev <sykidayteadres@gmail.com>
2017-11-13 17:45:13 +0200

    Новые изменения

---
+++ src/Chapter_3/SwitchDemo.java
@@ -0,0 +1,29 @@
+package Chapter_3;
+
+public class SwitchDemo {
+    public static void switchdemo()
+    {
+        int i;
+
+        for(i=0; i<10; i++)
+            switch(i){
+                case 0:
+                    System.out.println("i equal 0");
+                    break;
+                case 1:
+                    System.out.println("i equal 1");
+                    break;
+                case 2:
+                    System.out.println("i equal 2");
+                    break;
+                case 3:
+                    System.out.println("i equal 3");
+                    break;
+                case 4:
+                    System.out.println("i equal 4");
+                    break;
+
+            }
+    }
+
+}
