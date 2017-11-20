96adb98c7f010b67152562e2be6c5b80e1a0ffec
Vlad Kondratev <sykidayteadres@gmail.com>
2017-11-20 19:04:19 +0200

    Новый класс

---
+++ src/Chapter_3/ForTest.java
@@ -0,0 +1,13 @@
+package Chapter_3;
+
+public class ForTest {
+public static void fortest() throws java.io.IOException
+{
+    int i;
+
+    System.out.println("Press S to stop.");
+    for (i=0; (char) System.in.read() !='S'; i++)
+        System.out.println("i value is: "+ i);
+    
+}
+}
