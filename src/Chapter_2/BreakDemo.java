077b839228c8054e172c67fd002dd6877228b713
Vlad Kondratev <sykidayteadres@gmail.com>
2017-11-22 18:57:20 +0200

    New class

---
+++ src/Chapter_2/BreakDemo.java
@@ -0,0 +1,14 @@
+package Chapter_2;
+
+public class BreakDemo {
+    public static void breakdemo()
+    {
+        int num = 100;
+
+        for (int i = 0; i<num; i++){
+            if (i*i >=num)break;
+            System.out.println(i +" ");
+        }
+        System.out.println("Cycle finished.");
+    }
+}
