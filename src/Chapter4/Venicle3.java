0c988ddf5fb06c3cb977541a853c84655bad35ea
Vlad Kondratev <sykidayteadres@gmail.com>
2017-12-07 22:39:15 +0200

    Changes in class

--- src/Chapter4/Venicle3.java
+++ src/Chapter4/Venicle3.java
@@ -1,44 +1,44 @@
 package Chapter4;
 
 public class Venicle3 {
-    int passengers,fuelcap,mpg;
-    int range()
-    {
-       return mpg * fuelcap;
+    int passengers, fuelcap, mpg;
+
+    int range() {
+        return mpg * fuelcap;
     }
-double fuelneeded(int miles) {
-        return (double) miles/mpg;
+
+    Venicle3(int p, int f, int m) {
+        passengers = p;
+        fuelcap = f;
+        mpg = m;
+    }
+
+    double fuelneeded(int miles) {
+        return (double) miles / mpg;
+    }
 }
+
+
     class CompFuel {
         public void Venicle3L() {
-            Venicle3 minivan = new Venicle3();
-            Venicle3 sportscar = new Venicle3();
+            Venicle3 minivan = new Venicle3(7,16,21);
+            Venicle3 sportscar = new Venicle3(2,14,12);
 
             double gallons;
             int dist = 252;
-
-            int range1, range2;
-
-            minivan.passengers = 7;
-            minivan.fuelcap = 16;
-            minivan.mpg = 21;
-
-            sportscar.passengers = 2;
-            sportscar.fuelcap = 14;
-            minivan.mpg = 12;
-
             gallons = minivan.fuelneeded(dist);
+           System.out.println("Dlya preodolenia " + dist +
+                   "  miles minivan needed" + gallons +
+                   " fuel gallons");
 
-         System.out.println("To overcome " + dist + " miles minivan needs "+
-         gallons + " gallons fuel");
+           gallons = sportscar.fuelneeded(dist);
 
-         gallons = sportscar.fuelneeded(dist);
+           System.out.println("To overcome " + dist +
+                   " miles sportscar needed " + gallons +
+                   " fuel gallons");
 
-         System.out.println("To overcome " + dist+ " miles sportscar needed "+
-         gallons + " gallons fuel.");
 
 
         }
     }
 
-}
