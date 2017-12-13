4c0100291856da0d75ae953d72485f60028bbe75
Vlad Kondratev <sykidayteadres@gmail.com>
2017-12-13 15:02:52 +0200

    New class

---
+++ src/Chapter5/ArrayErr.java
@@ -0,0 +1,13 @@
+package Chapter5;
+
+public class ArrayErr {
+    public static void Launch()
+    {
+        int sample[] = new int[10];
+        int i;
+
+        for(i=0;i<100; i++)
+            sample[i] = i;
+    }
+
+}
