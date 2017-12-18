2b5476be99cfbf23f79698d973cab88d4328261d
Vlad Kondratev <sykidayteadres@gmail.com>
2017-12-18 10:44:52 +0200

    New classes

---
+++ src/Chapter5/NoChange.java
@@ -0,0 +1,17 @@
+package Chapter5;
+
+public class NoChange {
+    public static void Launch()
+    {
+        int nums[] = {1,2,3,4,5,6,7,8,9,10};{
+            for (int x: nums){
+                System.out.print(x+" ");
+                x = x*10;
+            }
+        System.out.println();
+        for (int x: nums)
+            System.out.print(x+" ");
+        System.out.println();
+        }
+    }
+}
