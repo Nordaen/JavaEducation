4ddab2d861becdb37e29816f2ab768b0be4f078f
Vlad Kondratev <sykidayteadres@gmail.com>
2017-12-20 20:29:51 +0200

    New class

---
+++ src/Chapter5/NoZeroDiv2.java
@@ -0,0 +1,12 @@
+package Chapter5;
+
+public class NoZeroDiv2 {
+    public static void Launch()
+    {
+
+        for(int i = 05; i<6; i++)
+            if(i !=0? true:false)
+                System.out.println("100 / " + i + " equal " + 100/i);
+        
+    }
+}
