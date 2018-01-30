c3cd068b916add90dc9ed779df484d0b4445ba47
Vlad Kondratev <sykidayteadres@gmail.com>
2018-01-30 14:37:44 +0200

    New class

---
+++ src/Chapter10/RandomAccessDemo.java
@@ -0,0 +1,37 @@
+package Chapter10;
+import java.io.*;
+public class RandomAccessDemo {
+
+    public static void Launch() throws IOException {
+        double data[] = {19.4,10.1,123.54,33.0,87.9,74.25};
+        double d;
+        try (RandomAccessFile raf = new RandomAccessFile("random.dat","rw")){
+            for (int i=0; i<data.length; i++){
+                raf.writeDouble(data[i]);
+            }
+            raf.seek(0);
+            d = raf.readDouble();
+            System.out.println("First value: " + d);
+
+            raf.seek(8);
+            d = raf.readDouble();
+            System.out.println("Second value: "+d);
+
+            raf.seek(8*3);
+            d = raf.readDouble();
+            System.out.println("Four value: " + d);
+
+            System.out.println();
+
+            System.out.println("Reading value with fuzzy serial numbers: ");
+            for (int i = 0; i<data.length; i+=2){
+                raf.seek(8*i);
+                d = raf.readDouble();
+                System.out.print(d + " ");
+            }
+        } catch(IOException exc){
+            System.out.println("Input-output error: " + exc);
+        }
+
+    }
+}
